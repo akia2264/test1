19ec5d4bef46b8c9b0e69e92980f6a5a61788094
akia2264 <90683713+akia2264@users.noreply.github.com>
2023-02-24 19:44:40 +0000

    Update traitement.py

--- traitement.py
+++ traitement.py
@@ -3,10 +3,20 @@ import cgi
 
 def calculer():
     form = cgi.FieldStorage()
-    nombre1 = int(form.getvalue('nombre1'))
-    nombre2 = int(form.getvalue('nombre2'))
+    nombre1 = form.getvalue('nombre1')
+    nombre2 = form.getvalue('nombre2')
+    
+    if not nombre1 or not nombre2:
+        return "Veuillez fournir deux nombres valides."
+    
+    try:
+        nombre1 = int(nombre1)
+        nombre2 = int(nombre2)
+    except ValueError:
+        return "Veuillez fournir deux nombres valides."
+    
     resultat = nombre1 * nombre2
-    return resultat
+    return str(resultat)
 
 print("Content-Type: text/html")
 print("")
